f524019dc0b74925018aa6a0e5c1441959fa6d94
Ky Nguyen <knguyen@enthusiastnetwork.com>
2021-05-05 18:08:50 -0500

    Initial commit

---
+++ src/main/java/piedpiper/bookzywebapp/BookzyWebappApplication.java
@@ -0,0 +1,13 @@
+package piedpiper.bookzywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookzyWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzyWebappApplication.class, args);
+	}
+
+}
